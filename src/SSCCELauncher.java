d6bfbeff047b5e625f3ff8b79dece6e3c3ade9f1
juniperbrew <ti.rasanen@gmail.com>
2015-06-20 22:55:53 +0300

    SSCCE

--- src/SSCCELauncher.java
+++ src/SSCCELauncher.java
@@ -11,8 +11,7 @@ import java.awt.event.ActionListener;
  */
 public class SSCCELauncher {
 
-
-    static boolean useJFrame = false;
+    static boolean useJFrame = true;
 
     public static void main(String[] args) {
 
@@ -24,8 +23,10 @@ public class SSCCELauncher {
 
             final JFrame frame = new JFrame();
             final SSCCEApplication application = new SSCCEApplication();
+            frame.setTitle(application.getClass().getSimpleName());
 
             final Canvas canvas = new Canvas();
+            canvas.setSize(cfg.width,cfg.height);
             frame.add(canvas);
 
             new LwjglApplication(application, cfg, canvas);
