d11b4cd95f0c199a40d148b76c76316cd39424ef
juniperbrew <ti.rasanen@gmail.com>
2015-06-25 18:20:52 +0300

    Stuff

--- scripts/MinimusServer.bat
+++ scripts/MinimusServer.bat
@@ -1,5 +1,18 @@
 @echo off
+Setlocal EnableDelayedExpansion
 
-cd minimus/build/install/Minimus/bin
+IF NOT EXIST Minimus\NUL ( 
+git clone https://github.com/Juniperbrew/Minimus
+cd Minimus
+call gradlew installDist
+) ELSE (
+cd Minimus
+set upToDate=
+for /f "delims=" %%a in ('git pull') do @set upToDate=%%a
+ECHO !upToDate!
+IF NOT "!upToDate!" == "Already up-to-date." call gradlew installDist
+)
+
+cd build/install/Minimus/bin
 cls
 MinimusServer
