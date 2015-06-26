3f51333d74c0f206cd55be2e478f04ccb3296057
juniperbrew <ti.rasanen@gmail.com>
2015-06-26 18:18:26 +0300

    Missing file from commit

--- src/com/juniperbrew/minimus/components/Rotation.java
+++ src/com/juniperbrew/minimus/components/Rotation.java
@@ -3,5 +3,12 @@ package com.juniperbrew.minimus.components;
 /**
  * Created by Juniperbrew on 26.6.2015.
  */
-public class Rotation {
+public class Rotation extends Component {
+    public int degrees;
+
+    public Rotation(){}
+
+    public Rotation(int degrees){
+        this.degrees = degrees;
+    }
 }
